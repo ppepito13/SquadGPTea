cc0e83268d9578075d360aff84843c369337bdfa
Mateusz <mateusz.wasiak@gmail.com>
2023-10-20 23:14:46 +0200

    formatted date

--- front/src/pages/kid/Post.tsx
+++ front/src/pages/kid/Post.tsx
@@ -5,6 +5,8 @@ import sadIcon from '../../assets/feel-like/2_sad.png';
 import neutralIcon from '../../assets/feel-like/3_neutral.png';
 import hepiIcon from '../../assets/feel-like/4_hepi.png';
 import veryHepiIcon from '../../assets/feel-like/5_very_hepi.png';
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 const feelLikeList = [
   {img:verySadIcon, value:0},
@@ -26,7 +28,9 @@ var imgNr;
           <Col><img src={feelLikeList.find(fll=>fll.value===post.feelLike)?.img} height='64' width='64'></img></Col>
         </Row>
         <Row>
-            <Col>{post.createdAt}</Col>
+            <Col>
+            <Moment date={post.createdAt} format="YYYY/MM/DD hh:mm"></Moment>
+            </Col>
         </Row>
       </Col>
       <Col size="auto">{post.comment}</Col>
