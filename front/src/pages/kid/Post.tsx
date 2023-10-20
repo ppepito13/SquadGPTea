3de4bc2b9b8d2ec1edf7464dc5e15e3922b083cb
Mateusz <mateusz.wasiak@gmail.com>
2023-10-20 23:02:21 +0200

    Created basic look of single "messaga/post/..."

--- front/src/pages/kid/Post.tsx
+++ front/src/pages/kid/Post.tsx
@@ -1,12 +1,40 @@
 import react from "react";
-import { Card } from "react-onsenui";
+import { Card, Col, Row } from "react-onsenui";
+import verySadIcon from '../../assets/feel-like/1_very_sad.png';
+import sadIcon from '../../assets/feel-like/2_sad.png';
+import neutralIcon from '../../assets/feel-like/3_neutral.png';
+import hepiIcon from '../../assets/feel-like/4_hepi.png';
+import veryHepiIcon from '../../assets/feel-like/5_very_hepi.png';
+
+const feelLikeList = [
+  {img:verySadIcon, value:0},
+  {img:sadIcon, value:1},
+  {img:neutralIcon, value:2},
+  {img:hepiIcon, value:3},
+  {img:veryHepiIcon, value:4}
+]
+
 
 const Post = ({post}:Props) =>{
-
+var imgNr;
   return(
+    
     <Card modifier="post">
-      <pre>{JSON.stringify(post, null, 2)}</pre>
+      <Row>
+      <Col size="auto">
+        <Row>
+          <Col><img src={feelLikeList.find(fll=>fll.value===post.feelLike)?.img} height='64' width='64'></img></Col>
+        
+        </Row>
+        <Row>
+            <Col><pre>{JSON.stringify(post.createdAt, null, 2)}</pre></Col>
+        </Row>
+      </Col>
+      <Col size="auto"><pre>{JSON.stringify(post.comment, null, 2)}</pre></Col>
+      </Row>
+      
     </Card>
+    
   )
 }
 
